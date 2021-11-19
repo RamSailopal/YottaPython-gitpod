bdaa11a447c79a0a75b20c4e809135cd4a5c6151
Raman Sailopal <thelinuxguys@yahoo.com>
2021-11-19 15:32:53 +0000

    Chaging for MOVIE global

--- data/mpython.py
+++ data/mpython.py
@@ -1,7 +1,7 @@
 import mg_python
 mg_python.m_set_host(0, "localhost", 7041, "", "")
-print("Setting global Test, subscript 1 to Test Entry...")
-mg_python.m_set(0, "^Test", 1, "Test Entry")
+print("Setting Up MOVIE global entry for hello world...")
+mg_python.m_set(0, "^MOVIE", "hello world", "")
 print('Reading ^Test(1)...')
 name = mg_python.m_get(0, "^Test", 1)
 print(name)
